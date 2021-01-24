10e4d5128f590a44fb8c872b718a25ad580876b0
Lucas <lucasvalenteds@gmail.com>
2021-01-24 19:17:47 -0300

    Testing PreparedStatement with string-based index

--- src/test/java/com/example/spring/MainTest.java
+++ src/test/java/com/example/spring/MainTest.java
@@ -3,12 +3,16 @@ package com.example.spring;
 import com.example.spring.testing.IntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.jdbc.BadSqlGrammarException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.SqlParameter;
 import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 
+import java.math.BigDecimal;
+import java.sql.CallableStatement;
 import java.sql.Types;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -19,6 +23,32 @@ public class MainTest extends IntegrationTest {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
+    @Test
+    public void testFailingToUsePreparedStatementWithNamedParameters() {
+        int johnId = getAccountId("John Smith");
+        int maryId = getAccountId("Mary Jane");
+        double amountToTransfer = 500d;
+
+        Throwable throwable = assertThrows(InvalidDataAccessApiUsageException.class, () ->
+            jdbcTemplate.call(connection -> {
+                CallableStatement statement = connection.prepareCall("CALL transfer(?, ?, ?)");
+                statement.setInt("source", maryId);
+                statement.setInt("target", johnId);
+                statement.setBigDecimal("amount", BigDecimal.valueOf(amountToTransfer));
+                return statement;
+            }, List.of(
+                new SqlParameter("source", Types.INTEGER),
+                new SqlParameter("target", Types.INTEGER),
+                new SqlParameter("amount", Types.DECIMAL)
+            )));
+
+        // https://github.com/pgjdbc/pgjdbc/blob/master/pgjdbc/src/main/java/org/postgresql/jdbc/PgCallableStatement.java#L710-L712
+        assertTrue(
+            throwable.getMessage()
+                .contains("Method org.postgresql.jdbc.PgCallableStatement.setInt(String,int) is not yet implemented.")
+        );
+    }
 
     @Test
     public void testFailingToUseSimpleJdbcCall() {
