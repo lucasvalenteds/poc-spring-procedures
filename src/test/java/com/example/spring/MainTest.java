09b6eeee066c3bce2dffef58105c5ae203e25cfb
Lucas <lucasvalenteds@gmail.com>
2021-01-24 21:01:41 -0300

    Testing EntityManager methods to call procedures

--- src/test/java/com/example/spring/MainTest.java
+++ src/test/java/com/example/spring/MainTest.java
@@ -1,14 +1,22 @@
 package com.example.spring;
 
+import com.example.spring.account.Account;
+import com.example.spring.account.AccountRepository;
 import com.example.spring.testing.IntegrationTest;
+import org.hibernate.testing.transaction.TransactionUtil;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.InvalidDataAccessApiUsageException;
+import org.springframework.dao.InvalidDataAccessResourceUsageException;
 import org.springframework.jdbc.BadSqlGrammarException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.SqlParameter;
 import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 
+import javax.persistence.EntityManager;
+import javax.persistence.ParameterMode;
+import javax.persistence.PersistenceException;
 import java.math.BigDecimal;
 import java.sql.CallableStatement;
 import java.sql.Types;
@@ -25,7 +33,14 @@ public class MainTest extends IntegrationTest {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    @Autowired
+    private EntityManager entityManager;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
     @Test
+    @Tag("JDBC")
     public void testPreparedStatementWithParameters() {
         int johnId = getAccountId("John Smith");
         int maryId = getAccountId("Mary Jane");
@@ -53,6 +68,7 @@ public class MainTest extends IntegrationTest {
     }
 
     @Test
+    @Tag("JDBC")
     public void testFailingToUsePreparedStatementWithNamedParameters() {
         int johnId = getAccountId("John Smith");
         int maryId = getAccountId("Mary Jane");
@@ -79,6 +95,7 @@ public class MainTest extends IntegrationTest {
     }
 
     @Test
+    @Tag("JDBC")
     public void testFailingToUseSimpleJdbcCall() {
         int johnId = getAccountId("John Smith");
         int maryId = getAccountId("Mary Jane");
@@ -104,6 +121,64 @@ public class MainTest extends IntegrationTest {
         assertTrue(throwable.getMessage().contains("To call a procedure, use CALL"));
     }
 
+    @Test
+    @Tag("JPA")
+    public void testFailingToUseProcedureAnnotation() {
+        Account john = accountRepository.findByPersonName("John Smith").orElseThrow();
+        Account mary = accountRepository.findByPersonName("Mary Jane").orElseThrow();
+        BigDecimal amountToTransfer = BigDecimal.valueOf(500d);
+
+        Throwable throwable = assertThrows(InvalidDataAccessResourceUsageException.class, () ->
+            accountRepository.transfer(mary.getId(), john.getId(), amountToTransfer)
+        );
+
+        // https://github.com/spring-projects/spring-framework/issues/26014
+        // https://github.com/pgjdbc/pgjdbc/issues/1413
+        assertTrue(getLatestExceptionThrown(throwable).getMessage().contains("To call a procedure, use CALL"));
+    }
+
+    @Test
+    @Tag("JPA")
+    public void testFailingToUseEntityManagerCreateStoredProcedureQuery() {
+        Account john = accountRepository.findByPersonName("John Smith").orElseThrow();
+        Account mary = accountRepository.findByPersonName("Mary Jane").orElseThrow();
+        BigDecimal amountToTransfer = BigDecimal.valueOf(500d);
+
+        Throwable throwable = assertThrows(PersistenceException.class, () ->
+            entityManager.createStoredProcedureQuery("transfer")
+                .registerStoredProcedureParameter(1, Integer.class, ParameterMode.IN)
+                .registerStoredProcedureParameter(2, Integer.class, ParameterMode.IN)
+                .registerStoredProcedureParameter(3, BigDecimal.class, ParameterMode.IN)
+                .setParameter(1, mary.getId())
+                .setParameter(2, john.getId())
+                .setParameter(3, amountToTransfer)
+                .execute()
+        );
+
+        assertTrue(getLatestExceptionThrown(throwable).getMessage().contains("To call a procedure, use CALL"));
+    }
+
+    @Test
+    @Tag("JPA")
+    public void testFailingToUseEntityManagerCreateNativeQuery() {
+        Account john = accountRepository.findByPersonName("John Smith").orElseThrow();
+        Account mary = accountRepository.findByPersonName("Mary Jane").orElseThrow();
+        BigDecimal amountToTransfer = BigDecimal.valueOf(500d);
+
+
+        Throwable throwable = assertThrows(PersistenceException.class, () ->
+            TransactionUtil.doInJPA(() -> entityManager.getEntityManagerFactory(), em -> {
+                em.createNativeQuery("call transfer(?, ?, ?)")
+                    .setParameter(1, mary.getId())
+                    .setParameter(2, john.getId())
+                    .setParameter(3, amountToTransfer)
+                    .executeUpdate();
+            })
+        );
+
+        assertTrue(getLatestExceptionThrown(throwable).getMessage().contains("ERROR: invalid transaction termination"));
+    }
+
     private int getAccountId(String personName) {
         Integer accountId = jdbcTemplate.queryForObject(
             "select id from accounts where owner = (select id from people where name = ?)",
@@ -116,7 +191,6 @@ public class MainTest extends IntegrationTest {
         return accountId;
     }
 
-
     private double getAccountBalance(int accountId) {
         Double balance = jdbcTemplate.queryForObject(
             "select balance from accounts where id = ?",
@@ -128,4 +202,15 @@ public class MainTest extends IntegrationTest {
 
         return balance;
     }
+
+    private Throwable getLatestExceptionThrown(Throwable throwable) {
+        Throwable cause = throwable.getCause();
+
+        if (cause == null) {
+            return throwable;
+        }
+
+        return getLatestExceptionThrown(cause);
+    }
+
 }
